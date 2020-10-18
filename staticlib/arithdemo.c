9262b04f6f313f45374d4882ce1c30cbda264a77
Swati Singh <12swati8@gmail.com>
2020-10-18 11:23:17 -0700

    Creating Static Library

---
+++ staticlib/arithdemo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "arith.h"
+
+int main(int argc,char const *argv[])
+{
+    printf("Addition:%d\n",add(10,20));
+    printf("Diff:%d\n",sub(10,20));
+
+    return 0;
+} 
