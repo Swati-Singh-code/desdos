1d688f7ba976e6c08bea021dec1ccea20d59a01a
Swati Singh <12swati8@gmail.com>
2020-10-17 23:23:21 -0700

    Demo for Makefile

--- buildprocess/builddemo.c
+++ buildprocess/builddemo.c
@@ -2,9 +2,11 @@
 
 int add (int , int);
 int sub (int , int);
+
 int main(int argc,char const *argv[])
 {
     printf("Addition:%d\n",add(10,20));
-    printf("diff:%d\n",sub(10,20));
+    printf("Diff:%d\n",sub(10,20));
+
     return 0;
 } 
