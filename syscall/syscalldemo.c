dbebec2a2b08c4a5448498c1e0c4dd692c603f6a
Swati Singh <12swati8@gmail.com>
2020-10-20 21:19:57 -0700

    File descriptor demo

---
+++ syscall/syscalldemo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
+#include <unistd.h>
+
+
+
+int main(int argc,char const *argv[])
+{
+    int fd;
+
+    fd = open("desd.txt", O_WRONLY | O_CREAT);
+
+    if (-1 == fd);
+    {
+        perror("Error in open: \n");
+        _exit;
+    }
+    write(fd, "cdac", 4);
+    close(fd);
+
+    return 0;
+}
