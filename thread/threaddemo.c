673a94d9b18279d69c0a418e1f95df33088ba4aa
Swati Singh <12swati8@gmail.com>
2020-10-28 23:22:14 -0700

    fairness policy Demo

--- thread/threaddemo.c
+++ thread/threaddemo.c
@@ -12,9 +12,8 @@ int main(int argc , char const *argv[])
     pthread_t tid;
     printf("Before thread creation\n");
     pthread_create(&tid, NULL, thread, NULL);
-    printf("After thread creation\n");
-    sleep(1);
-    printf("After sleep creation\n");
+    
+
     return 0;
 }
 
